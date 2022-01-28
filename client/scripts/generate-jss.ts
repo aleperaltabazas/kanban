5e6d01b3646cfc35c9becf16576e47290c82d668
Alejandro <aleperaltabazas@hotmail.com>
2022-01-28 20:48:01 -0300

    adding jss generation scripts

---
+++ client/scripts/generate-jss.ts
@@ -0,0 +1,53 @@
+const fs = require("fs");
+
+const size = 15;
+const sides = [
+  { abbrev: "l", side: "Left" },
+  { abbrev: "r", side: "Right" },
+  { abbrev: "b", side: "Bottom" },
+  { abbrev: "t", side: "Bottom" },
+];
+
+type Style = {
+  className: string;
+  style: {
+    key: string;
+    value: string;
+  };
+};
+
+const toStyleString = (s: Style) =>
+  `${s.className}:{${s.style.key}:"${s.style.value}"}`;
+
+function generate(property: string) {
+  const styles = [1, 2, 3, 4, 5]
+    .flatMap((p) =>
+      sides.map((s) => {
+        const className = `${property.charAt(0)}${s.abbrev}${p}`;
+        const key = `${property}${s.side}`;
+
+        return {
+          className: className,
+          style: {
+            key: key,
+            value: `${p * size}px`,
+          },
+        } as Style;
+      })
+    )
+    .reduce((acc, next) => `${acc}${toStyleString(next)},`, "");
+
+  return `
+import { createStyles } from "@mui/styles";
+
+const ${property} = createStyles({${styles.toString()}});
+
+export default ${property};
+    `;
+}
+
+const paddings = generate("padding");
+const margins = generate("margin");
+
+fs.writeFileSync("src/jss/margin.ts", margins);
+fs.writeFileSync("src/jss/padding.ts", paddings);
