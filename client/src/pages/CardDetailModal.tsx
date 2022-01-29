3d7e7bccd724dc287d3cd23f98b41ba88967d844
Alejandro <aleperaltabazas@hotmail.com>
2022-01-29 17:02:40 -0300

    added formik

--- client/src/pages/CardDetailModal.tsx
+++ client/src/pages/CardDetailModal.tsx
@@ -11,6 +11,7 @@ import {
   useCreateCardMutation,
   StatusInput,
   Task,
+  Label,
 } from "../generated/graphql";
 import { useSnackbar } from "../context/Snackbar";
 import CheckCircleIcon from "@mui/icons-material/CheckCircle";
@@ -31,6 +32,7 @@ import AddCircleIcon from "@mui/icons-material/AddCircle";
 import { makeStyles } from "@mui/styles";
 import baseStyles from "../styles";
 import classnames from "classnames";
+import { Formik, FormikErrors } from "formik";
 
 type CardModalProps = {
   card?: Card;
@@ -46,67 +48,47 @@ const CardDetailsModal = ({ card, status }: CardModalProps) => {
   const [, createCard] = useCreateCardMutation();
   const { showSnackbar } = useSnackbar();
   const { cards, setCards, disabled, setDisabled } = useBoard();
-  const [title, setTitle] = useState(card?.title);
-  const [description, setDescription] = useState(card?.description);
-  const [tasks, setTasks] = useState(card?.tasks || []);
-  const [labels, setLabels] = useState(card?.labels || []);
-  const [priority, setPriority] = useState(card?.priority);
   const [autofocusTarget, setAutofocusTarget] = useState<number | undefined>();
 
-  useEffect(() => {
-    return () => {
-      setTitle(undefined);
-      setDescription(undefined);
-      setPriority(0);
-      setTasks([]);
-      setLabels([]);
-    };
-  }, []);
-
-  const handleNewTask = () => {
-    setAutofocusTarget(tasks.length);
-    setTasks(
-      tasks.concat({
-        description: "",
-        priority: 0,
-        completed: false,
-      } as Task)
-    );
-  };
-
-  const saveChanges = async () => {
+  const saveChanges = async (values: {
+    title: string;
+    description: string;
+    priority: number;
+    tasks: Task[];
+    labels: Label[];
+  }) => {
     setDisabled(true);
     hideModal();
     let response: any = {};
     if (card) {
       const res = await updateCard({
         id: card.id,
-        title: title,
-        description: description,
-        tasks: tasks.map((t) => ({
+        title: values.title,
+        description: values.description,
+        tasks: values.tasks.map((t) => ({
           description: t.description,
           completed: t.completed,
           priority: t.priority,
         })),
-        labels: labels.map((l) => ({
+        labels: values.labels.map((l) => ({
           id: l.id,
           color: l.color,
           name: l.name,
         })),
-        priority: priority,
+        priority: values.priority,
       });
       response.errors = res.error;
       response.data = res.data.updateCard.card;
     } else {
       const res = await createCard({
-        title: title,
-        description: description,
-        tasks: tasks.map((t) => ({
+        title: values.title,
+        description: values.description,
+        tasks: values.tasks.map((t) => ({
           description: t.description,
           completed: t.completed,
           priority: t.priority,
         })),
-        labels: labels.map((l) => ({
+        labels: values.labels.map((l) => ({
           id: l.id,
           color: l.color,
           name: l.name,
@@ -143,121 +125,213 @@ const CardDetailsModal = ({ card, status }: CardModalProps) => {
       fullWidth
       maxWidth="md"
     >
-      <DialogContent>
-        <DialogContentText id="alert-dialog-description">
-          <Grid container spacing={4}>
-            <Grid item xs={12}>
-              <Typography variant="h6">Title</Typography>
-              <TextField
-                fullWidth
-                variant="standard"
-                value={title}
-                onChange={(e) => setTitle(e.currentTarget.value)}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <Typography variant="h6">Description</Typography>
-              <TextField
-                fullWidth
-                variant="standard"
-                value={description}
-                onChange={(e) => setDescription(e.currentTarget.value)}
-                multiline
-                rows={3}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <Typography variant="h6">Priority</Typography>
-              <TextField
-                fullWidth
-                variant="standard"
-                value={priority}
-                defaultValue={0}
-                inputProps={{ inputMode: "numeric", pattern: "[0-9]*" }}
-                onChange={(e) => {
-                  const n = Number.parseInt(e.currentTarget.value);
-                  setPriority(Number.isNaN(n) ? 0 : n);
-                }}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <Typography variant="h6">Tasks</Typography>
-              <List
-                sx={{
-                  width: "100%",
-                  bgcolor: "background.paper",
-                }}
-              >
-                {tasks.map((t, idx) => (
-                  <ListItem key={idx} disablePadding sx={{ width: "100%" }}>
-                    <ListItemButton role={undefined} onClick={() => {}} dense>
-                      <ListItemIcon>
-                        <Checkbox
-                          edge="start"
-                          checked={t.completed}
-                          tabIndex={-1}
-                          disableRipple
-                          onChange={() =>
-                            setTasks(
-                              tasks.map((t, i) =>
-                                i == idx
-                                  ? {
-                                      ...t,
-                                      completed: !t.completed,
-                                    }
-                                  : t
-                              )
-                            )
-                          }
-                        />
-                      </ListItemIcon>
-                      <ListItemText
-                        primary={
-                          <TextField
-                            autoFocus={autofocusTarget == idx}
-                            onFocus={() => setAutofocusTarget(undefined)}
-                            fullWidth
-                            variant="standard"
-                            value={t.description}
-                            onChange={(e) =>
-                              setTasks(
-                                tasks.map((t, i) =>
-                                  i == idx
-                                    ? {
-                                        ...t,
-                                        description: e.currentTarget.value,
-                                      }
-                                    : t
-                                )
-                              )
-                            }
-                          />
-                        }
-                      />
-                    </ListItemButton>
-                  </ListItem>
-                ))}
-              </List>
-              <Button
-                variant="contained"
-                startIcon={<AddCircleIcon />}
-                className={classnames(classes.textWhite, classes.mt1)}
-                onClick={handleNewTask}
-              >
-                new task
+      <Formik
+        initialValues={{
+          title: card?.title,
+          description: card?.description,
+          priority: card?.priority?.toString() || "0",
+          tasks: card?.tasks || [],
+          labels: card?.labels || [],
+        }}
+        validate={(values) => {
+          const errors: Partial<typeof values> = {};
+
+          if (!values.title) {
+            errors.title = "Title is required";
+          }
+
+          if (!values.priority) {
+            errors.priority = "Priority is required";
+          } else if (!values.priority.match(/^\d+$/)) {
+            errors.priority = "Priority must be a (positive) number";
+          }
+
+          values.tasks.forEach((task, i) => {
+            if (!task.description) {
+              if (!errors.tasks) {
+                errors.tasks = Array(values.tasks.length).fill(
+                  undefined as Task
+                );
+              }
+
+              errors.tasks[i] = {
+                description: "Task description is required",
+              } as Task;
+            }
+          });
+
+          return errors;
+        }}
+        onSubmit={(values) => {
+          saveChanges({
+            title: values.title,
+            description: values.description,
+            priority: Number.parseInt(values.priority),
+            tasks: values.tasks,
+            labels: values.labels,
+          });
+        }}
+      >
+        {({
+          values,
+          errors,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          setValues,
+        }) => (
+          <>
+            <DialogContent>
+              <DialogContentText id="alert-dialog-description">
+                <Grid container spacing={4}>
+                  <Grid item xs={12}>
+                    <Typography variant="h6">Title</Typography>
+                    <TextField
+                      fullWidth
+                      variant="standard"
+                      name="title"
+                      value={values.title}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      error={errors.title != undefined}
+                      helperText={errors.title}
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Typography variant="h6">Description</Typography>
+                    <TextField
+                      fullWidth
+                      name="description"
+                      variant="standard"
+                      value={values.description}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      multiline
+                      rows={3}
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Typography variant="h6">Priority</Typography>
+                    <TextField
+                      fullWidth
+                      name="priority"
+                      variant="standard"
+                      value={values.priority}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      helperText={errors.priority}
+                      error={errors.priority != undefined}
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Typography variant="h6">Tasks</Typography>
+                    <List
+                      sx={{
+                        width: "100%",
+                        bgcolor: "background.paper",
+                      }}
+                    >
+                      {values.tasks.map((t, idx) => (
+                        <ListItem
+                          key={idx}
+                          disablePadding
+                          sx={{ width: "100%" }}
+                        >
+                          <ListItemButton
+                            role={undefined}
+                            onClick={() => {}}
+                            dense
+                          >
+                            <ListItemIcon>
+                              <Checkbox
+                                edge="start"
+                                checked={t.completed}
+                                tabIndex={-1}
+                                disableRipple
+                                onChange={() =>
+                                  setValues({
+                                    ...values,
+                                    tasks: values.tasks.map((t, i) =>
+                                      i == idx
+                                        ? { ...t, completed: !t.completed }
+                                        : t
+                                    ),
+                                  })
+                                }
+                              />
+                            </ListItemIcon>
+                            <ListItemText
+                              primary={
+                                <TextField
+                                  autoFocus={autofocusTarget == idx}
+                                  onFocus={() => setAutofocusTarget(undefined)}
+                                  fullWidth
+                                  variant="standard"
+                                  value={t.description}
+                                  helperText={
+                                    errors.tasks != undefined &&
+                                    errors.tasks[idx] != undefined &&
+                                    (errors.tasks[idx] as FormikErrors<Task>)
+                                      .description
+                                  }
+                                  error={
+                                    errors.tasks != undefined &&
+                                    errors.tasks[idx] != undefined
+                                  }
+                                  onChange={(e) =>
+                                    setValues({
+                                      ...values,
+                                      tasks: values.tasks.map((t, i) =>
+                                        i == idx
+                                          ? {
+                                              ...t,
+                                              description:
+                                                e.currentTarget.value,
+                                            }
+                                          : t
+                                      ),
+                                    })
+                                  }
+                                />
+                              }
+                            />
+                          </ListItemButton>
+                        </ListItem>
+                      ))}
+                    </List>
+                    <Button
+                      variant="contained"
+                      startIcon={<AddCircleIcon />}
+                      className={classnames(classes.textWhite, classes.mt1)}
+                      onClick={() => {
+                        setAutofocusTarget(values.tasks.length);
+                        setValues({
+                          ...values,
+                          tasks: values.tasks.concat({
+                            description: "",
+                            priority: 0,
+                            completed: false,
+                          } as Task),
+                        });
+                      }}
+                    >
+                      new task
+                    </Button>
+                  </Grid>
+                </Grid>
+              </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={hideModal} color="error">
+                Cancel
               </Button>
-            </Grid>
-          </Grid>
-        </DialogContentText>
-      </DialogContent>
-      <DialogActions>
-        <Button onClick={hideModal} color="error">
-          Cancel
-        </Button>
-        <Button onClick={saveChanges} autoFocus color="primary">
-          Save
-        </Button>
-      </DialogActions>
+              <Button onClick={() => handleSubmit()} autoFocus color="primary">
+                Save
+              </Button>
+            </DialogActions>
+          </>
+        )}
+      </Formik>
     </Dialog>
   );
 };
